0f82155738fae0a79eaea3af961e3af291f4c660
patbi <biyagapatrick@gmail.com>
2024-01-24 18:05:29 +0100

    Module2 In Progress

--- cli/module2.dart
+++ cli/module2.dart
@@ -39,11 +39,35 @@ void main() {
 	//print(name);
 
 
-	print(Bank().shape);
-	print('${Bank().size}cm');
-	Bank().banking();
-	final isBankCooling = Bank().isCooling();
-	print(isBankCooling);
+	//print(Bank().shape);
+	//print('${Bank().size}cm');
+	//Bank().banking();
+	//final isBankCooling = Bank().isCooling();
+	//print(isBankCooling);
+
+
+	//final bank = Bank(shape: 'Hi', size: 24);
+	//print(bank.shape);
+
+	//print(bank.shape);
+	//print(bank.size);
+
+
+	//print(Constants.greeting);
+	//print(Constants.bye);
+	//print(Constants.giveMeSomeValue());
+
+	// Inheritance
+	// Object Oriented Programming (OOP)
+
+	Car car = Car();
+
+	//print(car.isEngineWorking);
+	//print(car.noOfWheels);
+
+	Vehicule tnvg = Car();
+	print(car.isEngineWorking);
+	print((car as Car).noOfWheels);
 
 }
 
@@ -98,16 +122,92 @@ String printPat() => 'See-Docs';
 
 class Bank {
 	// variables
-	String shape = 'Shape';
-	double size = 56;
+	//String shape = 'Shape';
+	//double size = 56;
+	
+
+	//String shape;
+	//double size;
+	//Bank(this.shape, this.size) {
+	//	print('Bank constructor called');
+	//	banking();
+	//}
+
+	//String? shape;
+	//double? size;
+	//Bank(this.shape, this.size) {
+	//	print('Bank constructor called');
+	//	banking();
+	//}
+
+	//String? shape = 'Bank';
+	//double? size;
+	//Bank(String shape, double size) {
+	//	print(this.shape);
+	//	this.shape = shape;
+	//	this.size = size;
+	//}
+
+
+	final String shape;
+	final double size;
+	Bank({required this.shape, required this.size}) {
+		banking();
+	}
+
+
 
 	// method
+	//void banking() {
+	//	print('Banking has started');
+	//}
+
+
 	void banking() {
-		print('Banking has started');
+		print('Your Bank of $shape shape and $size');
 	}
+
 
 	bool isCooling() {
 		return false;
 	}
 }
 
+
+
+class Constants {
+	static int height = 10;
+	static String greeting = 'Hello, how are you ?';
+	static String bye = 'Bye';
+
+	static int giveMeSomeValue() {
+		return height;
+	}
+}
+
+
+
+
+// Inheritance
+// Object Oriented Programming (OOP)
+
+class Vehicule {
+	//variables
+
+	int speed = 15;
+	bool isEngineWorking = false;
+	bool isLighOn = true;
+
+	//methods
+	void accelerate() {
+		speed+= 23;
+	}
+}
+
+class Car extends Vehicule {
+	int noOfWheels = 7;
+
+	void printSomething() {
+		print(noOfWheels);
+	}
+}
